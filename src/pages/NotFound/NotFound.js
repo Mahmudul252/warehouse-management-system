861a08a0a2a7c998bb36a7c9d2e8630a087441fe
mahmudulkabir <mahmudulkabir990@gmail.com>
2022-05-13 12:08:15 +0600

    not fpund page updated

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h3>404 Not Found..</h3>
+        <div className='text-danger text-center mt-5'>
+            <h1>OOPS!</h1>
+            <img className='w-50' src="https://i.ibb.co/1KH8Zpj/404.jpg" alt="" />
+            <h2>The page you are finding is not found..</h2>
         </div>
     );
 };
